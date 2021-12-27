0c925ab4911e54ef109b4bd2e30ed319538c0da3
huangst <313949553@qq.com>
2021-12-27 15:03:50 +0800

    fix：bug

--- src/pages/capacity/components/teamList.vue
+++ src/pages/capacity/components/teamList.vue
@@ -93,6 +93,7 @@
 				contentHeight: '860upx',
 				defaultPhoneHeight:'', //屏幕默认高度
 				nowPhoneHeight:'', //屏幕现在的高度
+				system:'ios'
 			}
 		},
 		watch: {
@@ -115,7 +116,9 @@
 			nowPhoneHeight(){
 				if(this.defaultPhoneHeight != this.nowPhoneHeight){
 					//手机键盘被唤起了。
-					this.contentHeight = '500upx'
+					if (this.system === 'android') {
+						this.contentHeight = '500upx'
+					}
 				}else{
 					//手机键盘被关闭了。
 					this.contentHeight = '860upx'
@@ -124,7 +127,9 @@
 		},
 		mounted() {
 			//监听软键盘获取当前屏幕高度的事件
-			this.defaultPhoneHeight = window.innerHeight
+			this.defaultPhoneHeight = window.innerHeight;
+			const res = uni.getSystemInfoSync();
+			this.system = res.platform;
 		},
 		methods: {
 			close() {
