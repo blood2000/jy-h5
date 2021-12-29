8b7b2e7fbe7f4ecc7e334627dd08a8424d0e7c38
chenhj <121459108@qq.com>
2021-12-29 14:21:51 +0800

    12

--- src/pages/transportPlan/index.vue
+++ src/pages/transportPlan/index.vue
@@ -329,21 +329,12 @@
 					query: `code=${this.cbData.orderPlanCode}&type=1`
 				};
 
-				console.log(que);
+				// console.log(que);
 
 				// 请求一下地址
 				buildQrCode(que, this.headerInfo).then(res=>{
-					console.log(res);
+					// console.log(res);
 					if (res.data.errmsg === 'ok') {
-						console.log({
-							shareUrl: res.data.url_link, // 分享连接
-							shareTitle:"承运链接", // 分享的标题
-							shareContent: `点此接【${ this.cbData.name }】的运单`, // 分享的描述
-							shareImg: this.logoBase64 || "http://qn.kemean.cn//upload/202004/18/1587189024467w6xj18b1.jpg",
-							appId: undefined, // 默认不传type的时候，必须传appId和appPath才会显示小程序图标
-							appPath: undefined,
-							appWebUrl: undefined
-						}); 
 						
 						// console.log('分享的连接是:' , this.qrcode.val);
 						this.sendOption('onShare', {
