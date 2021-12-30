669c83671c7a5f21253d352ab0e7cf080528ee4d
wengal <108786044@qq.com>
2021-12-31 00:39:55 +0800

    未提交隐藏

--- src/pages/weighRecord/index.vue
+++ src/pages/weighRecord/index.vue
@@ -1,331 +1,331 @@
-<template>
-	<view class="content-page" :style="{'--statusBar12': (statusBar12) + 'px' }">
-		<HeaderBar title="过磅记录" @back="navigateBack" :border="false"></HeaderBar>
-	  <!-- main -->
-	  <view class="main-box">
-			<!-- 筛选 -->
-			<view class="filter-wrap">
-				<view class="item-filter">
-					<!-- 选择时间 -->
-					<datetimerangePicker v-model="effectiveDate" />
-				</view>
-			</view>
-			<!-- 磅房列表 -->
-			<view class="list-wrap">
-				<scroll-view :scroll-top="scrollTop" scroll-y="true" class="scroll-Y" @scrolltolower="scrolltolower">
-					<view class="list-record" @click="navigateToDetail">
-						<view class="item-record" v-for="item in dataList" :key="item.id" :data-id="item.id">
-							<!-- 磅房标题 -->
-							<view class="item-head">
-								<text class="item-title">磅房A-地磅1</text>
-								<view class="item-head-right">
-									<i class="icon-arrow"></i>
-								</view>
-							</view>
-							<!-- 收发货统计 -->
-							<view class="item-weigh">
-								<view class="weigh-green">
-								<image src="../../static/weighRecord/icon_receipt.png" class="item-logo-green"></image>
-								<view class="wrapper-detail">
-									<view class="weigh-lable">收货：</view>
-									<view class="weigh-value">2车-26吨</view>
-								</view>
-								</view>
-								<view class="weigh-blue">
-								<image src="../../static/weighRecord/icon_delivery.png" class="item-logo-blue"></image>
-								<view class="wrapper-detail">
-									<view class="weigh-lable">发货：</view>
-									<view class="weigh-value">2车-26吨</view>
-								</view>
-								</view>
-							</view>
-							<view class="item-route building-top-line">
-								<view class="item-route-lastTime">
-									<text>最近一个过磅:</text>
-									<text>2021-12-12 18:18:40</text>
-								</view>
-								<view class="item-route-name">
-									<image src="../../static/weighRecord/icon_route.png" class="item-logo-route"></image>
-									<view class="item-route-place">
-										<text class="item-route-star">五福洗煤</text>
-										<i class="icon-logo-line"></i>
-										<text class="item-route-end">六福洗煤</text>
-									</view>
-								</view>
-								<view class="item-info">
-									<view class="item-info-car">
-										<view>
-											<text class="item-info-lable">毛重：</text>
-											<text class="item-info-value">8.23吨</text>
-										</view>
-									</view>
-									<view class="item-info-driver">
-										<view>
-											<text class="item-info-lable">车牌：</text>
-											<text class="item-info-value">闽A54772</text>
-										</view>
-										<view>
-											<text class="item-info-lable">司机：</text>
-											<text class="item-info-value">兔斯基</text>
-										</view>
-										</view>
-								</view>
-							</view>
-						</view>
-					</view>
-					<uni-load-more v-if="dataList && dataList.length > 0" :status="status" :icon-size="16" :content-text="contentText" />
-				</scroll-view>
-			</view>
-		</view>
-	</view>
-</template>
+<!--<template>-->
+<!--	<view class="content-page" :style="{'&#45;&#45;statusBar12': (statusBar12) + 'px' }">-->
+<!--		<HeaderBar title="过磅记录" @back="navigateBack" :border="false"></HeaderBar>-->
+<!--	  &lt;!&ndash; main &ndash;&gt;-->
+<!--	  <view class="main-box">-->
+<!--			&lt;!&ndash; 筛选 &ndash;&gt;-->
+<!--			<view class="filter-wrap">-->
+<!--				<view class="item-filter">-->
+<!--					&lt;!&ndash; 选择时间 &ndash;&gt;-->
+<!--					<datetimerangePicker v-model="effectiveDate" />-->
+<!--				</view>-->
+<!--			</view>-->
+<!--			&lt;!&ndash; 磅房列表 &ndash;&gt;-->
+<!--			<view class="list-wrap">-->
+<!--				<scroll-view :scroll-top="scrollTop" scroll-y="true" class="scroll-Y" @scrolltolower="scrolltolower">-->
+<!--					<view class="list-record" @click="navigateToDetail">-->
+<!--						<view class="item-record" v-for="item in dataList" :key="item.id" :data-id="item.id">-->
+<!--							&lt;!&ndash; 磅房标题 &ndash;&gt;-->
+<!--							<view class="item-head">-->
+<!--								<text class="item-title">磅房A-地磅1</text>-->
+<!--								<view class="item-head-right">-->
+<!--									<i class="icon-arrow"></i>-->
+<!--								</view>-->
+<!--							</view>-->
+<!--							&lt;!&ndash; 收发货统计 &ndash;&gt;-->
+<!--							<view class="item-weigh">-->
+<!--								<view class="weigh-green">-->
+<!--								<image src="../../static/weighRecord/icon_receipt.png" class="item-logo-green"></image>-->
+<!--								<view class="wrapper-detail">-->
+<!--									<view class="weigh-lable">收货：</view>-->
+<!--									<view class="weigh-value">2车-26吨</view>-->
+<!--								</view>-->
+<!--								</view>-->
+<!--								<view class="weigh-blue">-->
+<!--								<image src="../../static/weighRecord/icon_delivery.png" class="item-logo-blue"></image>-->
+<!--								<view class="wrapper-detail">-->
+<!--									<view class="weigh-lable">发货：</view>-->
+<!--									<view class="weigh-value">2车-26吨</view>-->
+<!--								</view>-->
+<!--								</view>-->
+<!--							</view>-->
+<!--							<view class="item-route building-top-line">-->
+<!--								<view class="item-route-lastTime">-->
+<!--									<text>最近一个过磅:</text>-->
+<!--									<text>2021-12-12 18:18:40</text>-->
+<!--								</view>-->
+<!--								<view class="item-route-name">-->
+<!--									<image src="../../static/weighRecord/icon_route.png" class="item-logo-route"></image>-->
+<!--									<view class="item-route-place">-->
+<!--										<text class="item-route-star">五福洗煤</text>-->
+<!--										<i class="icon-logo-line"></i>-->
+<!--										<text class="item-route-end">六福洗煤</text>-->
+<!--									</view>-->
+<!--								</view>-->
+<!--								<view class="item-info">-->
+<!--									<view class="item-info-car">-->
+<!--										<view>-->
+<!--											<text class="item-info-lable">毛重：</text>-->
+<!--											<text class="item-info-value">8.23吨</text>-->
+<!--										</view>-->
+<!--									</view>-->
+<!--									<view class="item-info-driver">-->
+<!--										<view>-->
+<!--											<text class="item-info-lable">车牌：</text>-->
+<!--											<text class="item-info-value">闽A54772</text>-->
+<!--										</view>-->
+<!--										<view>-->
+<!--											<text class="item-info-lable">司机：</text>-->
+<!--											<text class="item-info-value">兔斯基</text>-->
+<!--										</view>-->
+<!--										</view>-->
+<!--								</view>-->
+<!--							</view>-->
+<!--						</view>-->
+<!--					</view>-->
+<!--					<uni-load-more v-if="dataList && dataList.length > 0" :status="status" :icon-size="16" :content-text="contentText" />-->
+<!--				</scroll-view>-->
+<!--			</view>-->
+<!--		</view>-->
+<!--	</view>-->
+<!--</template>-->
 
-<script>
-	import HeaderBar from '@/components/Building/HeaderBar.vue';
-	import datetimerangePicker from './inc/DatetimerangePicker.vue'
-	export default {
-		components: {
-			HeaderBar,
-			datetimerangePicker
-		},
-		data() {
-			return {
-				scrollTop: 0,
-				statusBar12: 0,
-				oldDatePicker1: Date.now(), // 改变key值重新渲染
-				effectiveDate: [], // 转成 开始时间 和 结束时间
-				dataList: [{
-					id: 1
-				}, {
-					id: 2
-				}, {
-					id: 3
-				}],
-				status: 'more',
-				contentText: {
-					contentdown: '上拉加载更多',
-					contentrefresh: '加载中',
-					contentnomore: '没有更多了'
-				}
-			}
-		},
-		async onLoad(options) {
-			if(options.token){
-				this.$store.dispatch('getLoginInfoAction', {
-					'Authorization': options.token
-				});
-				options.statusBarHeight && this.$store.dispatch('getStatusBarHeightAction', options.statusBarHeight);
-			}
-			this.statusBar12 = this.statusBarHeight;
-			if(uni.getSystemInfoSync().platform == 'ios'){
-				this.statusBar12 -= 10
-			}
-		},
-		methods: {
-			/**
-			 * 跳转至过磅详情
-			 * @param {Object} e 当前点击对象
-			 */
-			navigateToDetail(e) {
-				console.log(11111)
-				if(e.target.dataset.id >= 0) {
-					uni.navigateTo({
-						url: `/pages/weighRecord/list?code=${e.target.dataset.id}`
-					});
-				}
-			},
-			// 触底
-			scrolltolower(e) {
-				console.log('触达底部')
-				if(!this.isEnd) {
-					this.status = 'loading';
-				}
-			},
-		}
-	}
-</script>
+<!--<script>-->
+<!--	import HeaderBar from '@/components/Building/HeaderBar.vue';-->
+<!--	import datetimerangePicker from './inc/DatetimerangePicker.vue'-->
+<!--	export default {-->
+<!--		components: {-->
+<!--			HeaderBar,-->
+<!--			datetimerangePicker-->
+<!--		},-->
+<!--		data() {-->
+<!--			return {-->
+<!--				scrollTop: 0,-->
+<!--				statusBar12: 0,-->
+<!--				oldDatePicker1: Date.now(), // 改变key值重新渲染-->
+<!--				effectiveDate: [], // 转成 开始时间 和 结束时间-->
+<!--				dataList: [{-->
+<!--					id: 1-->
+<!--				}, {-->
+<!--					id: 2-->
+<!--				}, {-->
+<!--					id: 3-->
+<!--				}],-->
+<!--				status: 'more',-->
+<!--				contentText: {-->
+<!--					contentdown: '上拉加载更多',-->
+<!--					contentrefresh: '加载中',-->
+<!--					contentnomore: '没有更多了'-->
+<!--				}-->
+<!--			}-->
+<!--		},-->
+<!--		async onLoad(options) {-->
+<!--			if(options.token){-->
+<!--				this.$store.dispatch('getLoginInfoAction', {-->
+<!--					'Authorization': options.token-->
+<!--				});-->
+<!--				options.statusBarHeight && this.$store.dispatch('getStatusBarHeightAction', options.statusBarHeight);-->
+<!--			}-->
+<!--			this.statusBar12 = this.statusBarHeight;-->
+<!--			if(uni.getSystemInfoSync().platform == 'ios'){-->
+<!--				this.statusBar12 -= 10-->
+<!--			}-->
+<!--		},-->
+<!--		methods: {-->
+<!--			/**-->
+<!--			 * 跳转至过磅详情-->
+<!--			 * @param {Object} e 当前点击对象-->
+<!--			 */-->
+<!--			navigateToDetail(e) {-->
+<!--				console.log(11111)-->
+<!--				if(e.target.dataset.id >= 0) {-->
+<!--					uni.navigateTo({-->
+<!--						url: `/pages/weighRecord/list?code=${e.target.dataset.id}`-->
+<!--					});-->
+<!--				}-->
+<!--			},-->
+<!--			// 触底-->
+<!--			scrolltolower(e) {-->
+<!--				console.log('触达底部')-->
+<!--				if(!this.isEnd) {-->
+<!--					this.status = 'loading';-->
+<!--				}-->
+<!--			},-->
+<!--		}-->
+<!--	}-->
+<!--</script>-->
 
-<style lang="scss" scoped>
-.content-page {
-	font-size: 28upx;
-	font-family: PingFang SC;
-	position: absolute;
-	top: 0;
-	left: 0;
-	width: 100%;
-	height: 100vh;
-	overflow: hidden;
-}
-.main-box{
-	height: calc(100% - 88upx);
-	padding: 24upx 30upx 0;
-	background: url(../../static/weighRecord/bg.png) #f5f5f5 no-repeat;
-	background-size: contain;
-	overflow: hidden;
-	display: flex;
-	flex-direction: column;
-}
-.filter-wrap {
-	width: 100%;
-	.item-filter {
-		display: flex;
-		align-items: center;
-	}
-}
-.list-wrap {
-	overflow-y: auto;
-	-webkit-overflow-scrolling: touch;
-	margin: 0 -30upx;
-	margin-top: 28upx;
-	padding: 0 30upx;
-	.scroll-Y {
-		height: 100%;
-	}	
-}
-.list-record {
-	.scroll-Y {
-		height: 100%;
-	}	
-	.item-record {
-		position: relative;
-		background-color: #fff;
-		border-radius: 24upx;
-		&::after {
-			content: "";
-			position: absolute;
-			left: 0;
-			right: 0;
-			bottom: 0;
-			top: 0;
-		}
-		&:not(:last-child) {
-			margin-bottom: 24upx;
-		}
-		.item-head {
-			height: 80upx;
-			display: flex;
-			justify-content: space-between;
-			align-items: center;
-			padding: 0 28upx;
-			font-size: 32rpx;
-			.item-title {
-				font-weight: bold;
-			}
-			.icon-arrow {
-				width: 18upx;
-				height: 26upx;
-				background: url(../../static/weighRecord/arrow_left.png) no-repeat;
-				background-size: contain;
-				display: block;
-				margin-left: 10upx;
-			}
-		}
-		.item-weigh {
-			display: flex;
-			justify-content: space-between;
-			padding: 0 28upx;
-			margin-top: 20upx;
-			.item-logo-green {
-				display: inline-block;
-				width: 70upx;
-				height: 70upx;
-				margin-right: 17upx;
-			}
-			.item-logo-blue {
-				display: inline-block;
-				width: 70upx;
-				height: 70upx;
-				margin-right: 17upx;
-			}
-			.wrapper-detail {
-				display: inline-block;
-			}
-			.weigh-blue {
-				margin-right: 60upx;
-			}
-			.weigh-lable {
-				font-size: 24upx;
-				color: #878787;
-			}
-			.weigh-value {
-				font-weight: bold;
-			}
-		}
-		.item-route {
-			position: relative;
-			margin-top: 40upx;
-			padding: 24upx;
-			background-color: rgba($color: #d6ddf5, $alpha: .2);
-			&::before {
-				position: absolute;
-				content: '';
-				width: 100%;
-				height: 1px;
-				left: 0;
-				top: 0;
-				background-color: #f0f0f0;
-				transform: scaleY(.5);
-			}
-			&-lastTime {
-				display: flex;
-				justify-content: space-between;
-				font-size: 24rpx;
-				color: #878787;
-			}
-			&-name {
-				display: flex;
-				align-items: center;
-				padding: 15upx 20upx;
-				background-color:rgba(204, 204, 204, .18);
-				border-radius: 6upx;
-				margin-top: 12upx;
-				.item-route-place {
-					display: flex;
-					justify-content: space-between;
-					align-items: center;
-					.item-route-star {
-						margin-left: 80upx;
-						margin-right: 50upx;
-						font-weight:bold;
-					}
-					.icon-logo-line {
-						width: 50upx;
-						height: 22upx;
-						background: url(../../static/weighRecord/icon_line.png) no-repeat;
-						background-size: contain;
-						display: block;
-						margin-left: 50upx;
-					}
-					.item-route-end {
-						margin-left: 80upx;
-						font-weight:bold;
-					}
-				}
-				.item-logo-route {
-					width: 35upx;
-					height:35upx;
-					margin-left: 20upx;
-					object-fit: contain;
-				}
-			}
-			.item-info {
-				margin-top: 15upx;
-				&-lable {
-					font-size: 24rpx;
-					color: #878787;
-				}
-				&-value {
-					font-size: 24rpx;
-					color: #333333;
-				}
-				&-driver {
-					display: flex;
-					justify-content: space-between;
-					align-items: center;
-					margin-top: 10upx;
-				}
-			}
-		}
-	}
-}
-</style>
+<!--<style lang="scss" scoped>-->
+<!--.content-page {-->
+<!--	font-size: 28upx;-->
+<!--	font-family: PingFang SC;-->
+<!--	position: absolute;-->
+<!--	top: 0;-->
+<!--	left: 0;-->
+<!--	width: 100%;-->
+<!--	height: 100vh;-->
+<!--	overflow: hidden;-->
+<!--}-->
+<!--.main-box{-->
+<!--	height: calc(100% - 88upx);-->
+<!--	padding: 24upx 30upx 0;-->
+<!--	background: url(../../static/weighRecord/bg.png) #f5f5f5 no-repeat;-->
+<!--	background-size: contain;-->
+<!--	overflow: hidden;-->
+<!--	display: flex;-->
+<!--	flex-direction: column;-->
+<!--}-->
+<!--.filter-wrap {-->
+<!--	width: 100%;-->
+<!--	.item-filter {-->
+<!--		display: flex;-->
+<!--		align-items: center;-->
+<!--	}-->
+<!--}-->
+<!--.list-wrap {-->
+<!--	overflow-y: auto;-->
+<!--	-webkit-overflow-scrolling: touch;-->
+<!--	margin: 0 -30upx;-->
+<!--	margin-top: 28upx;-->
+<!--	padding: 0 30upx;-->
+<!--	.scroll-Y {-->
+<!--		height: 100%;-->
+<!--	}	-->
+<!--}-->
+<!--.list-record {-->
+<!--	.scroll-Y {-->
+<!--		height: 100%;-->
+<!--	}	-->
+<!--	.item-record {-->
+<!--		position: relative;-->
+<!--		background-color: #fff;-->
+<!--		border-radius: 24upx;-->
+<!--		&::after {-->
+<!--			content: "";-->
+<!--			position: absolute;-->
+<!--			left: 0;-->
+<!--			right: 0;-->
+<!--			bottom: 0;-->
+<!--			top: 0;-->
+<!--		}-->
+<!--		&:not(:last-child) {-->
+<!--			margin-bottom: 24upx;-->
+<!--		}-->
+<!--		.item-head {-->
+<!--			height: 80upx;-->
+<!--			display: flex;-->
+<!--			justify-content: space-between;-->
+<!--			align-items: center;-->
+<!--			padding: 0 28upx;-->
+<!--			font-size: 32rpx;-->
+<!--			.item-title {-->
+<!--				font-weight: bold;-->
+<!--			}-->
+<!--			.icon-arrow {-->
+<!--				width: 18upx;-->
+<!--				height: 26upx;-->
+<!--				background: url(../../static/weighRecord/arrow_left.png) no-repeat;-->
+<!--				background-size: contain;-->
+<!--				display: block;-->
+<!--				margin-left: 10upx;-->
+<!--			}-->
+<!--		}-->
+<!--		.item-weigh {-->
+<!--			display: flex;-->
+<!--			justify-content: space-between;-->
+<!--			padding: 0 28upx;-->
+<!--			margin-top: 20upx;-->
+<!--			.item-logo-green {-->
+<!--				display: inline-block;-->
+<!--				width: 70upx;-->
+<!--				height: 70upx;-->
+<!--				margin-right: 17upx;-->
+<!--			}-->
+<!--			.item-logo-blue {-->
+<!--				display: inline-block;-->
+<!--				width: 70upx;-->
+<!--				height: 70upx;-->
+<!--				margin-right: 17upx;-->
+<!--			}-->
+<!--			.wrapper-detail {-->
+<!--				display: inline-block;-->
+<!--			}-->
+<!--			.weigh-blue {-->
+<!--				margin-right: 60upx;-->
+<!--			}-->
+<!--			.weigh-lable {-->
+<!--				font-size: 24upx;-->
+<!--				color: #878787;-->
+<!--			}-->
+<!--			.weigh-value {-->
+<!--				font-weight: bold;-->
+<!--			}-->
+<!--		}-->
+<!--		.item-route {-->
+<!--			position: relative;-->
+<!--			margin-top: 40upx;-->
+<!--			padding: 24upx;-->
+<!--			background-color: rgba($color: #d6ddf5, $alpha: .2);-->
+<!--			&::before {-->
+<!--				position: absolute;-->
+<!--				content: '';-->
+<!--				width: 100%;-->
+<!--				height: 1px;-->
+<!--				left: 0;-->
+<!--				top: 0;-->
+<!--				background-color: #f0f0f0;-->
+<!--				transform: scaleY(.5);-->
+<!--			}-->
+<!--			&-lastTime {-->
+<!--				display: flex;-->
+<!--				justify-content: space-between;-->
+<!--				font-size: 24rpx;-->
+<!--				color: #878787;-->
+<!--			}-->
+<!--			&-name {-->
+<!--				display: flex;-->
+<!--				align-items: center;-->
+<!--				padding: 15upx 20upx;-->
+<!--				background-color:rgba(204, 204, 204, .18);-->
+<!--				border-radius: 6upx;-->
+<!--				margin-top: 12upx;-->
+<!--				.item-route-place {-->
+<!--					display: flex;-->
+<!--					justify-content: space-between;-->
+<!--					align-items: center;-->
+<!--					.item-route-star {-->
+<!--						margin-left: 80upx;-->
+<!--						margin-right: 50upx;-->
+<!--						font-weight:bold;-->
+<!--					}-->
+<!--					.icon-logo-line {-->
+<!--						width: 50upx;-->
+<!--						height: 22upx;-->
+<!--						background: url(../../static/weighRecord/icon_line.png) no-repeat;-->
+<!--						background-size: contain;-->
+<!--						display: block;-->
+<!--						margin-left: 50upx;-->
+<!--					}-->
+<!--					.item-route-end {-->
+<!--						margin-left: 80upx;-->
+<!--						font-weight:bold;-->
+<!--					}-->
+<!--				}-->
+<!--				.item-logo-route {-->
+<!--					width: 35upx;-->
+<!--					height:35upx;-->
+<!--					margin-left: 20upx;-->
+<!--					object-fit: contain;-->
+<!--				}-->
+<!--			}-->
+<!--			.item-info {-->
+<!--				margin-top: 15upx;-->
+<!--				&-lable {-->
+<!--					font-size: 24rpx;-->
+<!--					color: #878787;-->
+<!--				}-->
+<!--				&-value {-->
+<!--					font-size: 24rpx;-->
+<!--					color: #333333;-->
+<!--				}-->
+<!--				&-driver {-->
+<!--					display: flex;-->
+<!--					justify-content: space-between;-->
+<!--					align-items: center;-->
+<!--					margin-top: 10upx;-->
+<!--				}-->
+<!--			}-->
+<!--		}-->
+<!--	}-->
+<!--}-->
+<!--</style>-->
