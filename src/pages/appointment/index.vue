554b12d4468dbff4f62ca16c1d5d28c951c02181
feng lian <bigpig8@qq.com>
2022-01-06 14:58:53 +0800

    预约-4

--- src/pages/appointment/index.vue
+++ src/pages/appointment/index.vue
@@ -8,12 +8,19 @@
 					<view class="flex align-center">
 						<view class="size32 text-bold">{{userInfo.userName}}</view>
 					</view>
-					<view class="text-tag size20 margin-stop">{{userInfo.licenseNumber?userInfo.licenseNumber:'暂无'}}</view>
+					<view class="text-tag size20 margin-stop">{{userInfo.licenseNumber?userInfo.licenseNumber:'暂无'}}
+					</view>
 				</view>
 			</view>
-			<view class="scanView">
-				<image class="top-avatar shadow-warp bg-white" :src="avatar" mode="aspectFill"></image>
+			<view class="scanView" @click="onClickScanAction()">
+				<image class="top-scaner shadow-warp bg-white" src="/static/icon_station.png" mode="aspectFit"></image>
+				<view class="text-tag size20 margin-stop">
+					扫码预约
+				</view>
 			</view>
+		</view>
+		<view class="info-container">
+			
 		</view>
 	</view>
 </template>
@@ -37,11 +44,13 @@
 					userName: '张三',
 					licenseNumber: '闽A54332'
 				},
-				avatar: '/static/avatar.png',// 默认头像
+				avatar: '/static/avatar.png', // 默认头像
 			}
 		},
 		methods: {
-
+			onClickScanAction() {
+				console.log("点击了扫码");
+			},
 		}
 	}
 </script>
@@ -76,10 +85,14 @@
 	}
 
 	.scanView {
-		background: #00FF00;
+		background: #FFFFFF;
 		width: 140upx;
 		height: 140upx;
 		border-radius: 10upx;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
 	}
 
 	.top-avatar {
@@ -87,5 +100,22 @@
 		width: 88upx;
 		border-radius: 50%;
 		margin-left: 20upx;
+	}
+
+	.top-scaner {
+		height: 68upx;
+		width: 68upx;
+	}
+	
+	.info-container {
+		background: #FFFFFF;
+		margin-left: 20upx;
+		margin-right: 20upx;
+		margin-top: 20upx;
+		height: 200upx;
+		display: flex;
+		align-items: center;
+		flex-direction: row;
+		justify-content: space-between;
 	}
 </style>
