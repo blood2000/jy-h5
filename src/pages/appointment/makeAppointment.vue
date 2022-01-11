1b8086e187925c58d19cb6ae25ae7f00ffec83f0
feng lian <bigpig8@qq.com>
2022-01-11 14:36:45 +0800

    预约-3

--- src/pages/appointment/makeAppointment.vue
+++ src/pages/appointment/makeAppointment.vue
@@ -1,6 +1,33 @@
 <template>
 	<view class="content-page" :style="{'--statusBar12': (statusBar12) + 'px' }">
 		<HeaderBar title="预约凭证信息" @back="navigateBack" :border="false"></HeaderBar>
+		<view class="home-page">
+			<view class="companyView">
+				<image class="companyView_icon" src="/static/appointment/appointment_company.png" mode="aspectFill">
+				</image>
+				<text class="companyView_label">{{appointmentInfo.companyName}}</text>
+			</view>
+			<view class="stationView">
+				<image class="companyView_icon" src="/static/appointment/appointment_station.png" mode="aspectFill">
+				</image>
+				<view class="stationView_list">
+					<text class="stationView_nameLabel">{{appointmentInfo.stationName}}</text>
+					<text class="stationView_label">货品类型：{{appointmentInfo.goodsType}}</text>
+					<text class="stationView_label">预约时段：{{appointmentInfo.dateStr}}</text>
+					<text class="stationView_label">可预约数：{{appointmentInfo.canAppointmentCount}}</text>
+					<view class="stationView_viewMoreView">
+						<text class="stationView_label">已承运数：{{appointmentInfo.haveShipCount}}</text>
+						<text class="stationView_viewMorelabel">查看出入区信息</text>
+					</view>
+					
+				</view>
+			</view>
+			<view class="timeView">
+				<image class="companyView_icon" src="/static/appointment/appointment_time.png" mode="aspectFill">
+				</image>
+				<text class="stationView_nameLabel">场站现可预约时段</text>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -20,8 +47,7 @@
 					dateStr: '2021-12-29～2021-12-29',
 					canAppointmentCount: '10086',
 					haveShipCount: '99',
-					canAppointmentTime:[
-						{
+					canAppointmentTime: [{
 							timeStr: '9:00',
 							isAvailable: false
 						},
@@ -45,4 +71,90 @@
 </script>
 
 <style>
+	
+	.home-page {
+		display: flex;
+		align-items: flex-start;
+		flex-direction: column;
+		justify-content: space-between;
+	}
+	
+	.companyView {
+		display: flex;
+		align-items: center;
+		flex-direction: row;
+		justify-content: space-between;
+		margin-left: 24upx;
+		margin-right: 24upx;
+		margin-top: 45upx;
+	}
+	
+	.companyView_icon {
+		width: 58upx;
+		height: 58upx;
+		flex-shrink: 0;
+	}
+	
+	.companyView_label {
+		font-size: 32upx;
+		font-weight: bold;
+		color: #333333;
+		padding-left: 13upx;
+	}
+	
+	.stationView {
+		display: flex;
+		align-items: flex-start;
+		flex-direction: row;
+		justify-content: space-between;
+		margin-left: 24upx;
+		margin-right: 24upx;
+		margin-top: 51upx;
+	}
+	
+	.stationView_list {
+		display: flex;
+		align-items: flex-start;
+		flex-direction: column;
+		justify-content: space-between;
+	}
+	
+	.stationView_nameLabel {
+		font-size: 32upx;
+		font-weight: bold;
+		color: #333333;
+		padding-left: 13upx;
+		padding-top: 8upx;
+	}
+	
+	.stationView_label {
+		font-size: 28upx;
+		color: #333333;
+		padding-left: 13upx;
+		padding-top: 23upx;
+	}
+	
+	.stationView_viewMoreView {
+		display: flex;
+		align-items: center;
+		flex-direction: row;
+		justify-content: center;
+	}
+	
+	.stationView_viewMorelabel {
+		font-size: 24upx;
+		color: #2366F2;
+		padding-left: 19upx;
+		padding-top: 23upx;
+	}
+	
+	.timeView {
+		display: flex;
+		align-items: flex-start;
+		flex-direction: row;
+		justify-content: space-between;
+		margin-left: 24upx;
+		margin-right: 24upx;
+		margin-top: 54upx;
+	}
 </style>
