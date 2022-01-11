aa4bc3939d2aee97d796038befff959f9600850d
feng lian <bigpig8@qq.com>
2022-01-11 17:04:25 +0800

    预约-4

--- src/pages/appointment/makeAppointment.vue
+++ src/pages/appointment/makeAppointment.vue
@@ -19,13 +19,16 @@
 						<text class="stationView_label">已承运数：{{appointmentInfo.haveShipCount}}</text>
 						<text class="stationView_viewMorelabel">查看出入区信息</text>
 					</view>
-					
 				</view>
 			</view>
 			<view class="timeView">
 				<image class="companyView_icon" src="/static/appointment/appointment_time.png" mode="aspectFill">
 				</image>
 				<text class="stationView_nameLabel">场站现可预约时段</text>
+			</view>
+			<view class="form-button ly-flex ly-flex-pack-justify ly-flex-align-center">
+				<view class="cancel" @click="handleCancle('form')">取消</view>
+				<view class="sure" @click="handleSubmit('form')">确认预约</view>
 			</view>
 		</view>
 	</view>
@@ -71,6 +74,13 @@
 </script>
 
 <style>
+	.content-page {
+		background: #FFFFFF; 
+		background-size:100% 100%;
+		width:100%;
+		height:100%;
+		position:fixed;
+	}
 	
 	.home-page {
 		display: flex;
@@ -78,7 +88,7 @@
 		flex-direction: column;
 		justify-content: space-between;
 	}
-	
+
 	.companyView {
 		display: flex;
 		align-items: center;
@@ -88,20 +98,20 @@
 		margin-right: 24upx;
 		margin-top: 45upx;
 	}
-	
+
 	.companyView_icon {
 		width: 58upx;
 		height: 58upx;
 		flex-shrink: 0;
 	}
-	
+
 	.companyView_label {
 		font-size: 32upx;
 		font-weight: bold;
 		color: #333333;
 		padding-left: 13upx;
 	}
-	
+
 	.stationView {
 		display: flex;
 		align-items: flex-start;
@@ -111,14 +121,14 @@
 		margin-right: 24upx;
 		margin-top: 51upx;
 	}
-	
+
 	.stationView_list {
 		display: flex;
 		align-items: flex-start;
 		flex-direction: column;
 		justify-content: space-between;
 	}
-	
+
 	.stationView_nameLabel {
 		font-size: 32upx;
 		font-weight: bold;
@@ -126,28 +136,28 @@
 		padding-left: 13upx;
 		padding-top: 8upx;
 	}
-	
+
 	.stationView_label {
 		font-size: 28upx;
 		color: #333333;
 		padding-left: 13upx;
 		padding-top: 23upx;
 	}
-	
+
 	.stationView_viewMoreView {
 		display: flex;
 		align-items: center;
 		flex-direction: row;
 		justify-content: center;
 	}
-	
+
 	.stationView_viewMorelabel {
 		font-size: 24upx;
 		color: #2366F2;
 		padding-left: 19upx;
 		padding-top: 23upx;
 	}
-	
+
 	.timeView {
 		display: flex;
 		align-items: flex-start;
@@ -156,5 +166,39 @@
 		margin-left: 24upx;
 		margin-right: 24upx;
 		margin-top: 54upx;
+	}
+	
+	/*表单按钮css*/
+	.form-button{
+		position: fixed;
+		left: 0;
+		right: 0;
+		bottom: 0;
+		padding: 24upx 24upx 42upx;
+		background: #fff;
+	}
+	
+	.cancel, .sure{
+		height: 90upx;
+		line-height: 76upx;
+		border-radius: 10upx;
+		text-align: center;
+		font-size: 32upx;
+		font-family: PingFang SC;
+		font-weight: bold;
+		color: #3A65FF;
+	}
+	
+	.cancel{
+		width: 48%;
+		background: #FFFFFF; 
+		border: 1upx solid #3A65FF;
+		color: #3A65FF;
+	}
+	.sure{
+		width: 48%;
+		background: #3A65FF;
+		border: 1upx solid #3A65FF;
+		color: #fff;
 	}
 </style>
