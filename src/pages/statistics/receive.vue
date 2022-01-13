4a45cce7b9419a78768acc0ddb484a11df1ad1b9
wujb <jaw.bin@qq.com>
2022-01-13 16:42:52 +0800

    修复：收发货统计》筛选未完成状态参数错误

--- src/pages/statistics/receive.vue
+++ src/pages/statistics/receive.vue
@@ -297,6 +297,7 @@
 			},
 			bindPickerChange(e) {
 				this.clearQuery();
+				this.queryParams.status = e.target.value == 0 ? '' : 30;
 				this.queryParams = {
 					...this.queryParams,
 					pageNum: 1,
